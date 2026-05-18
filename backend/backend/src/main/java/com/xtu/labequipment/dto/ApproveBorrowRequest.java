4ea548490c520fb9dae4dfefc64aff61cddb743a
xtulyc <2976004383@qq.com>
2026-05-18 14:17:37 +0800

    Delete backend/backend directory

--- backend/backend/src/main/java/com/xtu/labequipment/dto/ApproveBorrowRequest.java
+++
@@ -1,9 +0,0 @@
-package com.xtu.labequipment.dto;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-@Data
-public class ApproveBorrowRequest {
-    @NotNull(message = "applyId required") private Long applyId;
-    @NotNull(message = "status required") private Integer status;
-    private String approveRemark;
-}
--- backend/backend/src/main/java/com/xtu/labequipment/dto/ReturnDeviceRequest.java
+++
@@ -1,8 +0,0 @@
-package com.xtu.labequipment.dto;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-@Data
-public class ReturnDeviceRequest {
-    @NotNull(message = "recordId required") private Long recordId;
-    private String remark;
-}
